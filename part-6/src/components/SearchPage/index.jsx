620fe742cc59d005afedc90622a404dbe3128b3f
Cas <cssamonte@gmail.com>
2023-10-27 11:29:27 -0700

    starter code

--- part-6/src/components/SearchPage/index.jsx
+++ part-6/src/components/SearchPage/index.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react"
+
 export default function SearchPage() {
-    return <h1>Search Page</h1>
+    // Store search term and API data here
+    const [query, setQuery] = useState('')
+
+    
+
+    return (
+        <div className="search-page p-10">
+            <form className="mt-4 text-center">
+                <label htmlFor="search" className="block font-medium mb-1">
+                    <h1 className="text-3xl font-bold">Search for Art</h1>
+                </label>
+                <br />
+                <input
+                    className="p-2 w-[60vw] rounded border border-gray-300 focus:outline-none focus:border-gray-500"
+                    name="search"
+                    placeholder="beautiful landscape..."
+                    value={query}
+                    onChange={}
+                />
+                <button
+                    type="submit"
+                    className="mx-1 px-4 py-2 text-gray-300 hover:text-white hover:bg-gray-800 bg-gray-700 rounded transition-all duration-200"
+                >
+                    Search
+                </button>
+            </form>
+        </div>
+    )
 }
